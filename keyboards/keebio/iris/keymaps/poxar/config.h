c97383ed13cdf4669f9fbc07fb30aeb7fc36c719
Philipp Millar <philipp.millar@poxar.net>
2021-07-16 01:59:25 +0200

    Add initial configuration for corne
    Additionally makes the two layouts compatible, since I plan to use the
    Corne on the got, while the Iris stays on my desk.

--- keyboards/keebio/iris/keymaps/poxar/config.h
+++ keyboards/keebio/iris/keymaps/poxar/config.h
@@ -4,6 +4,10 @@
 #define USE_SERIAL
 #define EE_HANDS
 
+#define TAPPING_TERM 150
+#define IGNORE_MOD_TAP_INTERRUPT
+#define RETRO_TAPPING
+
 #undef RGBLED_NUM
 #define RGBLED_NUM 12
 #define RGBLIGHT_SLEEP
--- keyboards/keebio/iris/keymaps/poxar/keymap.c
+++ keyboards/keebio/iris/keymaps/poxar/keymap.c
@@ -1,35 +1,46 @@
 #include QMK_KEYBOARD_H
 
-#define KC_CENT LCTL(KC_ENT)
+#define ENT_LWR LT(2, KC_ENT)
+#define ENT_GMG LT(4, KC_ENT)
+#define SPC_RSE LT(1, KC_SPC)
+#define ALT_TAB LALT(KC_TAB)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT(
       KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-      KC_LGUI, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                             KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MINS,        KC_EQL,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                                          KC_LALT, KC_LCTL, LGUI_T(KC_ENT), KC_SPC,  MO(1),   KC_RALT
+      KC_GESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                             KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MINS,         KC_EQL, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                                          KC_LALT, KC_LCTL, ENT_LWR,        SPC_RSE, KC_LGUI, KC_RALT
     ),
 	[1] = LAYOUT(
-      KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                            KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL,
-      KC_GRV,  KC_LCBR, KC_RCBR, KC_VOLD, KC_VOLU, KC_MUTE,                          KC_PSCR, KC_END,  KC_HOME, KC_LBRC, KC_RBRC, KC_INS,
-      KC_TRNS, KC_LPRN, KC_RPRN, KC_MPRV, KC_MNXT, KC_MPLY,                          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO,   RESET,
-      KC_TRNS, KC_LT,   KC_GT,   KC_NO,   KC_NO,   KC_LCBR, KC_F11,         KC_F12,  KC_RCBR, KC_PGDN, KC_PGUP, KC_NO,   KC_NO,   KC_TRNS,
-                                          KC_TRNS, KC_TRNS, LGUI(KC_ENT),   KC_PSCR, KC_TRNS, DF(2)
+      KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                            KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_BSPC,
+      KC_TRNS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,
+      KC_TRNS, KC_NO,   KC_NO,   KC_VOLD, KC_VOLU, KC_MUTE,                          KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,KC_NO,   KC_INS,
+      KC_TRNS, KC_NO,   KC_NO,   KC_MPRV, KC_MNXT, KC_MPLY, KC_F11,          KC_F12, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_NO,   RESET,
+                                          KC_TRNS, KC_TRNS, KC_NO,            KC_NO, KC_TRNS, DF(3)
     ),
-	[2] = LAYOUT(
+    [2] = LAYOUT(
+      KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                            KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_BSPC,
+      ALT_TAB, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                          KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
+      KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                            KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, KC_GRV,
+      KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,            KC_NO, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD,
+                                          KC_TRNS, KC_TRNS, KC_NO,            KC_NO, KC_TRNS, DF(3)
+    ),
+    // GAMING layer
+	[3] = LAYOUT(
       KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-      KC_LGUI, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                             KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+      KC_GESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                             KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MINS,        KC_EQL,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                                          KC_LALT, KC_LCTL, KC_SPC,         KC_ENT,  MO(3),   KC_RALT
+                                          KC_LALT, KC_LCTL, KC_SPC,        ENT_GMG,  KC_LGUI, KC_RALT
     ),
-	[3] = LAYOUT(
+	[4] = LAYOUT(
       KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                            KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL,
-      KC_GRV,  KC_LCBR, KC_RCBR, KC_VOLD, KC_VOLU, KC_MUTE,                          KC_PSCR, KC_END,  KC_HOME, KC_LBRC, KC_RBRC, KC_INS,
-      KC_TRNS, KC_LPRN, KC_RPRN, KC_MPRV, KC_MNXT, KC_MPLY,                          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO,   RESET,
-      KC_TRNS, KC_LT,   KC_GT,   KC_NO,   KC_NO,   KC_LCBR, KC_F11,         KC_F12,  KC_RCBR, KC_PGDN, KC_PGUP, KC_NO,   KC_NO,   KC_TRNS,
-                                          KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS, KC_TRNS, DF(0)
+      KC_TRNS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,
+      KC_TRNS, KC_NO,   KC_NO,   KC_VOLD, KC_VOLU, KC_MUTE,                          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO,   KC_INS,
+      KC_TRNS, KC_LT,   KC_GT,   KC_MPRV, KC_MNXT, KC_MPLY,  KC_F11,         KC_F12, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_NO,   KC_NO,
+                                          KC_TRNS, KC_TRNS, KC_TRNS,          KC_NO, KC_TRNS, DF(0)
     ),
 
 };
@@ -45,7 +56,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 rgblight_disable_noeeprom();
             }
             return true;
-        case DF(2):
+        case DF(3):
             if (record->event.pressed) {
                 rgblight_sethsv_noeeprom(0, 100, 100);
                 rgblight_enable_noeeprom();
