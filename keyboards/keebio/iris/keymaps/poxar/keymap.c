dfbb08354e54012be7ee58f9d4f313bdbfc09f9b
Philipp Millar <philipp.millar@poxar.net>
2023-06-14 19:23:29 +0200

    Implement homerow mods with training wheels

--- keyboards/keebio/iris/keymaps/poxar/keymap.c
+++ keyboards/keebio/iris/keymaps/poxar/keymap.c
@@ -16,13 +16,18 @@
 #define SPC_RSE LT(_RAISE, KC_SPACE)
 #define ENT_LWR LT(_LOWER, KC_ENTER)
 
-// One shot modifiers
-#define OS_LCTL OSM(MOD_LCTL)
-#define OS_LALT OSM(MOD_LALT)
-#define OS_LGUI OSM(MOD_LGUI)
-#define OS_RALT OSM(MOD_RALT)
-#define OS_LSFT OSM(MOD_LSFT)
-#define OS_RSFT OSM(MOD_RSFT)
+// Homerow modifiers
+#define HM_A MT(MOD_LGUI, KC_A)
+#define HM_S MT(MOD_LALT, KC_S)
+#define HM_D MT(MOD_LSFT, KC_D)
+#define HM_F MT(MOD_LCTL, KC_F)
+#define HM_G MT(MOD_RALT, KC_G)
+
+#define HM_H MT(MOD_RALT, KC_H)
+#define HM_J MT(MOD_RCTL, KC_J)
+#define HM_K MT(MOD_RSFT, KC_K)
+#define HM_L MT(MOD_LALT, KC_L)
+#define HM_SCLN MT(MOD_RGUI, KC_SCLN)
 
 // Macros
 #define KC_MICM KC_F20 // F20 mutes the microphone in linux
@@ -41,41 +46,41 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
 // ┌────────┬────────┬────────┬────────┬────────┬────────┐                    ┌────────┬────────┬────────┬────────┬────────┬────────┐
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
 // ├────────┼────────┼────────┼────────┼────────┼────────┤                    ├────────┼────────┼────────┼────────┼────────┼────────┤
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
 // ├────────┼────────┼────────┼────────┼────────┼────────┤                    ├────────┼────────┼────────┼────────┼────────┼────────┤
-    KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_ESC,  HM_A,    HM_S,    HM_D,    HM_F,    HM_G,                         HM_H,    HM_J,    HM_K,    HM_L,    HM_SCLN, KC_QUOT,
 // ├────────┼────────┼────────┼────────┼────────┼────────┼────────┐  ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-    OS_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC,    KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, OS_RSFT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC,    KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
 // └────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘  └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                   OS_LALT, OS_LCTL, ENT_LWR,             SPC_RSE, OS_LGUI, OS_RALT
+                                   KC_LALT, KC_LCTL, ENT_LWR,             SPC_RSE, KC_RCTL, KC_RALT
                                // └────────┴────────┴────────┘           └────────┴────────┴────────┘
   ),
   [_RAISE] = LAYOUT(
 // ┌────────┬────────┬────────┬────────┬────────┬────────┐                    ┌────────┬────────┬────────┬────────┬────────┬────────┐
-    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
 // ├────────┼────────┼────────┼────────┼────────┼────────┤                    ├────────┼────────┼────────┼────────┼────────┼────────┤
     _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PSCR,
 // ├────────┼────────┼────────┼────────┼────────┼────────┤                    ├────────┼────────┼────────┼────────┼────────┼────────┤
-    _______, PX_APP1, PX_APP2, DSK_LFT, DSK_RGT, XXXXXXX,                      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_P,    KC_INS,
+    _______, PX_APP1, PX_APP2, DSK_LFT, DSK_RGT, XXXXXXX,                      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT, KC_INS,
 // ├────────┼────────┼────────┼────────┼────────┼────────┼────────┐  ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-    _______, XXXXXXX, XXXXXXX, SFT_LFT, SFT_RGT, XXXXXXX, KC_MICM,    _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_TAB,  DF_GAME,
+    _______, XXXXXXX, XXXXXXX, SFT_LFT, SFT_RGT, XXXXXXX, KC_MICM,    _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_GRV,  DF_GAME,
 // └────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘  └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                    _______, _______, PX_CLR,              _______, _______, _______
                                // └────────┴────────┴────────┘           └────────┴────────┴────────┘
   ),
   [_LOWER] = LAYOUT(
 // ┌────────┬────────┬────────┬────────┬────────┬────────┐                    ┌────────┬────────┬────────┬────────┬────────┬────────┐
-    QK_BOOT, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_BSPC,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
 // ├────────┼────────┼────────┼────────┼────────┼────────┤                    ├────────┼────────┼────────┼────────┼────────┼────────┤
     _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
 // ├────────┼────────┼────────┼────────┼────────┼────────┤                    ├────────┼────────┼────────┼────────┼────────┼────────┤
-    _______, KC_Q,    PX_TILE, KC_VOLD, KC_VOLU, KC_MUTE,                      KC_BSPC, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_GRV,
+    _______, XXXXXXX, PX_TILE, KC_VOLD, KC_VOLU, KC_MUTE,                      KC_BSPC, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_GRV,
 // ├────────┼────────┼────────┼────────┼────────┼────────┼────────┐  ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
     _______, XXXXXXX, XXXXXXX, KC_MPRV, KC_MNXT, KC_MPLY, _______,    _______, KC_DEL,  KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_TILD,
 // └────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘  └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                   _______, _______, _______,             PX_CLR,  _______, _______
+                                   _______, KC_DEL,  _______,             PX_CLR,  KC_DEL,  QK_BOOT
                                // └────────┴────────┴────────┘           └────────┴────────┴────────┘
   ),
   [_GAMING] = LAYOUT(
@@ -105,6 +110,36 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                // └────────┴────────┴────────┘           └────────┴────────┴────────┘
   )
 };
+
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case HM_A:
+        case HM_S:
+        case HM_L:
+        case HM_SCLN:
+            return TAPPING_TERM + 50;
+        case HM_G:
+        case HM_H:
+            return TAPPING_TERM + 40;
+        case HM_D:
+        case HM_F:
+        case HM_J:
+        case HM_K:
+            return TAPPING_TERM + 30;
+        default:
+            return TAPPING_TERM;
+    }
+}
+
+uint16_t get_quick_tap_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case ENT_LWR:
+        case SPC_RSE:
+            return TAPPING_TERM;
+        default:
+            return 0;
+    }
+}
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
