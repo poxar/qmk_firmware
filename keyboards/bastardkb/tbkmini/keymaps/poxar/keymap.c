b11d097a2548034b9e3d9950db6e6fd8742b6f99
Philipp Millar <philipp.millar@poxar.net>
2024-01-11 20:15:05 +0100

    Snapshot

--- keyboards/bastardkb/tbkmini/keymaps/poxar/keymap.c
+++ keyboards/bastardkb/tbkmini/keymaps/poxar/keymap.c
@@ -20,43 +20,44 @@
 // Modtap keys
 #define CTL_TAB MT(MOD_LCTL, KC_TAB)
 #define INT_BSP MT(MOD_RALT, KC_BACKSPACE)
+#define GUI_ESC MT(MOD_LGUI, KC_ESC)
+#define ALT_DEL MT(MOD_LALT, KC_DEL)
+#define CTL_ESC MT(MOD_RCTL, KC_ESC)
 
-#define HM_S MT(MOD_LGUI, KC_S)
-#define HM_D MT(MOD_LALT, KC_D)
-#define HM_F MT(MOD_LCTL, KC_F)
-
-#define HM_J MT(MOD_RCTL, KC_J)
-#define HM_K MT(MOD_LALT, KC_K)
-#define HM_L MT(MOD_RGUI, KC_L)
+// Callum one shot mods
+#define OSM_CTL OSM(MOD_LCTL)
+#define OSM_ALT OSM(MOD_LALT)
+#define OSM_GUI OSM(MOD_LGUI)
+#define OSM_INT OSM(MOD_RALT)
 
 // Macros
-#define CTLBSPC LCTL(KC_BSPC)
 #define KC_MICM KC_F20 // F20 mutes the microphone in linux
 
 enum custom_keycodes {
-    PX_TILE = SAFE_RANGE,
+    PX_COPY = SAFE_RANGE,
+    PX_PAST,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_BASE] = LAYOUT_split_3x6_3(
 // ┌────────┬────────┬────────┬────────┬────────┬────────┐  ┌────────┬────────┬────────┬────────┬────────┬────────┐
-    KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
 // ├────────┼────────┼────────┼────────┼────────┼────────┤  ├────────┼────────┼────────┼────────┼────────┼────────┤
-    KC_ESC,  KC_A,    HM_S,    HM_D,    HM_F,    KC_G,       KC_H,    HM_J,    HM_K,    HM_L,    KC_SCLN, KC_QUOT,
+    KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
 // ├────────┼────────┼────────┼────────┼────────┼────────┤  ├────────┼────────┼────────┼────────┼────────┼────────┤
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
 // └────────┴────────┴────────┼────────┼────────┼────────┤  ├────────┼────────┼────────┴────────┴────────┴────────┘
-                               CTL_TAB, LT_LWR,  KC_DEL,     KC_ESC,  LT_RSE,  INT_BSP
+                               CTL_TAB, LT_LWR,  KC_LALT,    KC_LGUI, LT_RSE,  INT_BSP
                            // └────────┴────────┴────────┘  └────────┴────────┴────────┘
   ),
   [_RAISE] = LAYOUT_split_3x6_3(
 // ┌────────┬────────┬────────┬────────┬────────┬────────┐  ┌────────┬────────┬────────┬────────┬────────┬────────┐
-    _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,
 // ├────────┼────────┼────────┼────────┼────────┼────────┤  ├────────┼────────┼────────┼────────┼────────┼────────┤
-    _______, KC_GRV,  KC_BSLS, KC_LBRC, KC_LPRN, KC_LCBR,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT, _______,
+    _______, KC_GRV,  KC_BSLS, KC_LBRC, KC_LPRN, KC_LCBR,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_MNXT, _______,
 // ├────────┼────────┼────────┼────────┼────────┼────────┤  ├────────┼────────┼────────┼────────┼────────┼────────┤
-    _______, KC_TILD, KC_PIPE, KC_RBRC, KC_RPRN, KC_RCBR,    KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_DQUO, _______,
+    _______, KC_TILD, KC_PIPE, KC_RBRC, KC_RPRN, KC_RCBR,    KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_MPLY, _______,
 // └────────┴────────┴────────┼────────┼────────┼────────┤  ├────────┼────────┼────────┴────────┴────────┴────────┘
                                _______, OSL_ADJ, _______,    _______, _______, _______
                            // └────────┴────────┴────────┘  └────────┴────────┴────────┘
@@ -65,9 +66,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // ┌────────┬────────┬────────┬────────┬────────┬────────┐  ┌────────┬────────┬────────┬────────┬────────┬────────┐
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
 // ├────────┼────────┼────────┼────────┼────────┼────────┤  ├────────┼────────┼────────┼────────┼────────┼────────┤
-    _______, KC_ESC,  PX_TILE, KC_VOLD, KC_VOLU, KC_MUTE,    KC_PSCR, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______,
+    _______, OSM_INT, OSM_GUI, OSM_ALT, OSM_CTL, KC_VOLU,    KC_PSCR, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______,
 // ├────────┼────────┼────────┼────────┼────────┼────────┤  ├────────┼────────┼────────┼────────┼────────┼────────┤
-    _______, KC_DEL,  KC_MICM, KC_MPRV, KC_MNXT, KC_MPLY,    KC_INS,  KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, _______,
+    _______, KC_PSCR, KC_MICM, PX_COPY, PX_PAST, KC_VOLD,    KC_INS,  KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, _______,
 // └────────┴────────┴────────┼────────┼────────┼────────┤  ├────────┼────────┼────────┴────────┴────────┴────────┘
                                _______, _______, _______,    _______, OSL_ADJ, KC_DEL
                            // └────────┴────────┴────────┘  └────────┴────────┴────────┘
@@ -91,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // ├────────┼────────┼────────┼────────┼────────┼────────┤  ├────────┼────────┼────────┼────────┼────────┼────────┤
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
 // └────────┴────────┴────────┼────────┼────────┼────────┤  ├────────┼────────┼────────┴────────┴────────┴────────┘
-                               KC_LALT,  KC_SPC, OS_GRSE,    DF_BASE, KC_ENT,  KC_RALT
+                               KC_LCTL,  KC_SPC, OS_GRSE,    DF_BASE, KC_ENT,  KC_RALT
                            // └────────┴────────┴────────┘  └────────┴────────┴────────┘
   ),
   [_GAMING_RAISE] = LAYOUT_split_3x6_3(
@@ -102,44 +103,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // ├────────┼────────┼────────┼────────┼────────┼────────┤  ├────────┼────────┼────────┼────────┼────────┼────────┤
     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_I,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
 // └────────┴────────┴────────┼────────┼────────┼────────┤  ├────────┼────────┼────────┴────────┴────────┴────────┘
-                               KC_LCTL, _______, _______,    _______, _______, _______
+                               KC_LALT, _______, _______,    _______, _______, _______
                            // └────────┴────────┴────────┘  └────────┴────────┴────────┘
   ),
 };
 
-uint16_t get_quick_tap_term(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case LT_RSE:
-        case LT_LWR:
-        case INT_BSP:
-        case CTL_TAB:
-            return TAPPING_TERM;
-        default:
-            return 0;
-    }
-}
-
-uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case HM_S:
-        case HM_L:
-            return TAPPING_TERM + 100;
-        default:
-            return TAPPING_TERM;
-    }
-}
-
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case PX_TILE:
+        case PX_COPY:
             if (record->event.pressed) {
-                SEND_STRING(SS_LGUI(SS_TAP(X_RIGHT)));
-                wait_ms(10);
-                SEND_STRING(SS_LALT(SS_TAP(X_TAB)));
-                wait_ms(10);
-                SEND_STRING(SS_LGUI(SS_TAP(X_LEFT)));
-                wait_ms(10);
-                SEND_STRING(SS_LALT(SS_TAP(X_TAB)));
+                SEND_STRING(SS_LCTL(SS_TAP(X_C)));
+            }
+            break;
+        case PX_PAST:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LCTL(SS_TAP(X_V)));
             }
             break;
     }
