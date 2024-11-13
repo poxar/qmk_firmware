f70f0b1856320012a186b5f90058fe05e11a03e3
Philipp Millar <philipp.millar@poxar.net>
2024-11-13 19:19:22 +0100

    Try homerow mods one more time

--- keyboards/bastardkb/tbkmini/keymaps/poxar/config.h
+++ keyboards/bastardkb/tbkmini/keymaps/poxar/config.h
@@ -3,11 +3,17 @@
 #define USB_POLLING_INTERVAL_MS 1
 
 #define TAPPING_TERM 150
-#define HOLD_ON_OTHER_KEY_PRESS
+#define TAPPING_TERM_PER_KEY
+
+// Disable quick tap
+#define QUICK_TAP_TERM 0
+#define QUICK_TAP_TERM_PER_KEY
+
+// Allow for faster mod activation
+#define PERMISSIVE_HOLD_PER_KEY
 
 #define ONESHOT_TAP_TOGGLE 2
-#define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
-#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+#define ONESHOT_TIMEOUT 0
 
 #define EE_HANDS
 #define USB_VBUS_PIN GP19
--- keyboards/bastardkb/tbkmini/keymaps/poxar/keymap.c
+++ keyboards/bastardkb/tbkmini/keymaps/poxar/keymap.c
@@ -1,38 +1,51 @@
 #include QMK_KEYBOARD_H
 
 // Layers
-#define _BASE 0
-#define _RIGHT 1
-#define _LEFT 2
-#define _ADJUST 3
-#define _GAMING 4
-#define _GAMING_ONE 5
-#define _GAMING_TWO 6
+#define _STD 0
+#define _NAV 1
+#define _SYM 2
+#define _NUM 3
+#define _ADJ 4
+#define _GAMING 5
+#define _GAMING_ONE 6
+#define _GAMING_TWO 7
 
 // Layer switching
-#define DF_BASE DF(_BASE)
+#define OSL_NAV OSL(_NAV)
+#define OSL_SYM OSL(_SYM)
+#define OSL_NUM OSL(_NUM)
+#define OSL_ADJ OSL(_ADJ)
+
+#define DF_BASE DF(_STD)
 #define DF_GAME DF(_GAMING)
 #define OSG_ONE OSL(_GAMING_ONE)
 #define OSG_TWO OSL(_GAMING_TWO)
 
-#define OSL_RGT OSL(_RIGHT)
-#define OSL_LFT OSL(_LEFT)
-#define OSL_ADJ OSL(_ADJUST)
+// Homerow Mods
+#define HM_A MT(MOD_LSFT, KC_A)
+#define HM_S MT(MOD_LGUI, KC_S)
+#define HM_D MT(MOD_LALT, KC_D)
+#define HM_F MT(MOD_LCTL, KC_F)
 
-#define RIGHT_L LT(_RIGHT, KC_SPACE)
-#define LEFT_L LT(_LEFT, KC_SPACE)
+#define HM_J MT(MOD_RCTL, KC_J)
+#define HM_K MT(MOD_LALT, KC_K)
+#define HM_L MT(MOD_RGUI, KC_L)
+#define HM_SCLN MT(MOD_RSFT, KC_SCLN)
 
-// TAP
-#define MY_RSFT RSFT_T(KC_ENTER)
+// international characters on us keyboard
+#define HM_G MT(MOD_RALT, KC_G)
+#define HM_H MT(MOD_RALT, KC_H)
 
 // Modifiers
-#define OSM_CTL OSM(MOD_LCTL)
-#define OSM_ALT OSM(MOD_LALT)
-#define OSM_INT OSM(MOD_RALT)
-#define OSM_GUI OSM(MOD_LGUI)
+#define CAPSTOG QK_CAPS_WORD_TOGGLE
 
 // Shortcuts
 #define KC_MICM KC_F20 // F20 mutes the microphone in linux
+
+// Macros
+enum custom_keycodes {
+    MA_MAIL = SAFE_RANGE,
+};
 
 // Cancel oneshot with esc
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -60,51 +73,67 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         return rc;
     }
 
+    if (keycode == MA_MAIL && record->event.pressed) {
+      SEND_STRING("philipp.millar@poxar.net");
+      return true;
+    }
+
     return true;
 }
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  [_BASE] = LAYOUT_split_3x6_3(
+  [_STD] = LAYOUT_split_3x6_3(
 // ┌────────┬────────┬────────┬────────┬────────┬────────┐  ┌────────┬────────┬────────┬────────┬────────┬────────┐
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
 // ├────────┼────────┼────────┼────────┼────────┼────────┤  ├────────┼────────┼────────┼────────┼────────┼────────┤
-    KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_ESC,  HM_A,    HM_S,    HM_D,    HM_F,    HM_G,       HM_H,    HM_J,    HM_K,    HM_L,    HM_SCLN, KC_QUOT,
 // ├────────┼────────┼────────┼────────┼────────┼────────┤  ├────────┼────────┼────────┼────────┼────────┼────────┤
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MY_RSFT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
 // └────────┴────────┴────────┼────────┼────────┼────────┤  ├────────┼────────┼────────┴────────┴────────┴────────┘
-                               OSM_CTL, LEFT_L,  OSM_ALT,    KC_LGUI, RIGHT_L, OSM_INT
+                               KC_ESC,  OSL_SYM, OSL_NUM,    OSL_NAV, KC_SPC,  QK_REP
+//                            └────────┴────────┴────────┘  └────────┴────────┴────────┘
+  ),
+  [_NAV] = LAYOUT_split_3x6_3(
+// ┌────────┬────────┬────────┬────────┬────────┬────────┐  ┌────────┬────────┬────────┬────────┬────────┬────────┐
+    KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
+// ├────────┼────────┼────────┼────────┼────────┼────────┤  ├────────┼────────┼────────┼────────┼────────┼────────┤
+    _______, XXXXXXX, KC_MICM, KC_VOLD, KC_VOLU, KC_MUTE,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_INS,  _______,
+// ├────────┼────────┼────────┼────────┼────────┼────────┤  ├────────┼────────┼────────┼────────┼────────┼────────┤
+    _______, XXXXXXX, XXXXXXX, KC_MPRV, KC_MNXT, KC_MPLY,    KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_PSCR, _______,
+// └────────┴────────┴────────┼────────┼────────┼────────┤  ├────────┼────────┼────────┴────────┴────────┴────────┘
+                               _______, OSL_ADJ, _______,    _______, _______, _______
                            // └────────┴────────┴────────┘  └────────┴────────┴────────┘
   ),
-  [_RIGHT] = LAYOUT_split_3x6_3(
+  [_SYM] = LAYOUT_split_3x6_3(
 // ┌────────┬────────┬────────┬────────┬────────┬────────┐  ┌────────┬────────┬────────┬────────┬────────┬────────┐
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
+    KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,    KC_CIRC, KC_AMPR, KC_ASTR, XXXXXXX, XXXXXXX, _______,
 // ├────────┼────────┼────────┼────────┼────────┼────────┤  ├────────┼────────┼────────┼────────┼────────┼────────┤
-    _______, QK_AREP, KC_MICM, KC_VOLD, KC_VOLU, KC_MUTE,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, OSL_LFT, KC_INS,
+    _______, CAPSTOG, KC_LCBR, KC_LBRC, KC_LPRN, KC_LT,      XXXXXXX, KC_MINS, KC_EQL,  KC_BSLS, KC_PIPE, _______,
 // ├────────┼────────┼────────┼────────┼────────┼────────┤  ├────────┼────────┼────────┼────────┼────────┼────────┤
-    _______, QK_REP,  XXXXXXX, KC_MPRV, KC_MNXT, KC_MPLY,    KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_PSCR, _______,
+    CAPSTOG, KC_DEL,  KC_RCBR, KC_RBRC, KC_RPRN, KC_GT,      XXXXXXX, KC_UNDS, KC_PLUS, XXXXXXX, XXXXXXX, _______,
 // └────────┴────────┴────────┼────────┼────────┼────────┤  ├────────┼────────┼────────┴────────┴────────┴────────┘
-                               _______, OSL_ADJ, OSM_INT,    _______, _______, _______
+                               _______, _______, OSL_NAV,    OSL_ADJ, KC_BSPC, QK_AREP
                            // └────────┴────────┴────────┘  └────────┴────────┴────────┘
   ),
-  [_LEFT] = LAYOUT_split_3x6_3(
+  [_NUM] = LAYOUT_split_3x6_3(
 // ┌────────┬────────┬────────┬────────┬────────┬────────┐  ┌────────┬────────┬────────┬────────┬────────┬────────┐
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
+    KC_TAB,  XXXXXXX, KC_7,    KC_8,    KC_9,    XXXXXXX,    KC_SLSH, KC_KP_7, KC_KP_8, KC_KP_9, KC_MINS, _______,
 // ├────────┼────────┼────────┼────────┼────────┼────────┤  ├────────┼────────┼────────┼────────┼────────┼────────┤
-    _______, OSL_RGT, KC_LBRC, KC_LCBR, KC_LPRN, OSM_GUI,    XXXXXXX, KC_MINS, KC_EQL,  KC_BSLS, KC_PIPE, _______,
+    _______, KC_0,    KC_1,    KC_2,    KC_3,    XXXXXXX,    KC_ASTR, KC_KP_4, KC_KP_5, KC_KP_6, KC_PLUS, _______,
 // ├────────┼────────┼────────┼────────┼────────┼────────┤  ├────────┼────────┼────────┼────────┼────────┼────────┤
-    _______, KC_DEL,  KC_RBRC, KC_RCBR, KC_RPRN, KC_PSCR,    XXXXXXX, KC_UNDS, KC_PLUS, XXXXXXX, XXXXXXX, _______,
+    _______, XXXXXXX, KC_4,    KC_5,    KC_6,    XXXXXXX,    XXXXXXX, KC_KP_1, KC_KP_2, KC_KP_3, KC_ENT,  _______,
 // └────────┴────────┴────────┼────────┼────────┼────────┤  ├────────┼────────┼────────┴────────┴────────┴────────┘
-                               _______, _______, _______,    _______, OSL_ADJ, _______
+                               DF_GAME, _______, _______,    KC_KP_0, KC_SPC,  KC_KP_DOT
                            // └────────┴────────┴────────┘  └────────┴────────┴────────┘
   ),
-  [_ADJUST] = LAYOUT_split_3x6_3(
+  [_ADJ] = LAYOUT_split_3x6_3(
 // ┌────────┬────────┬────────┬────────┬────────┬────────┐  ┌────────┬────────┬────────┬────────┬────────┬────────┐
-    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   XXXXXXX,    KC_SLSH, KC_7,    KC_8,    KC_9,    KC_MINS, _______,
+    XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,   XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
 // ├────────┼────────┼────────┼────────┼────────┼────────┤  ├────────┼────────┼────────┼────────┼────────┼────────┤
-    _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   XXXXXXX,    KC_ASTR, KC_4,    KC_5,    KC_6,    KC_PLUS, _______,
+    XXXXXXX, KC_F5,   KC_F6,   KC_F7,   KC_F8,   XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
 // ├────────┼────────┼────────┼────────┼────────┼────────┤  ├────────┼────────┼────────┼────────┼────────┼────────┤
-    _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX,    KC_0,    KC_1,    KC_2,    KC_3,    KC_ENT,  _______,
+    XXXXXXX, KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX,    XXXXXXX, MA_MAIL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
 // └────────┴────────┴────────┼────────┼────────┼────────┤  ├────────┼────────┼────────┴────────┴────────┴────────┘
                                DF_GAME, _______, _______,    _______, _______, QK_BOOT
                            // └────────┴────────┴────────┘  └────────┴────────┴────────┘
@@ -143,3 +172,61 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                            // └────────┴────────┴────────┘  └────────┴────────┴────────┘
   ),
 };
+
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case HM_A:
+        case HM_SCLN:
+        case KC_LSFT:
+            return TAPPING_TERM + 100;
+
+        case HM_S:
+        case HM_L:
+            return TAPPING_TERM + 50;
+
+        case HM_D:
+        case HM_F:
+        case HM_J:
+        case HM_K:
+            return TAPPING_TERM;
+
+        default:
+            return TAPPING_TERM;
+    }
+}
+
+uint16_t get_quick_tap_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case OSL_NUM:
+        case OSL_ADJ:
+        case OSL_NAV:
+        case OSL_SYM:
+        case OSG_ONE:
+        case OSG_TWO:
+            return 500;
+        default:
+            return 0;
+    }
+}
+
+bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+    for (uint8_t i = led_min; i < led_max; i++) {
+        switch(get_highest_layer(layer_state|default_layer_state)) {
+            case _SYM:
+            case _GAMING_ONE:
+                rgb_matrix_set_color(i, RGB_BLUE);
+                break;
+            case _NUM:
+            case _GAMING_TWO:
+                rgb_matrix_set_color(i, RGB_GREEN);
+                break;
+            case _GAMING:
+            case _ADJ:
+                rgb_matrix_set_color(i, RGB_RED);
+                break;
+            default:
+                break;
+        }
+    }
+    return false;
+}
